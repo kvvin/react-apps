ad61a79b4837472f17f98ae51c7e803e5a50a290
Kevin Thomas Manjooran <kevinthomastm2002@gmail.com>
2024-02-18 20:17:56 +0530

    click me files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+  const [counter, setCounter] = useState(0);
 
+  let increamentCounter = () =>{
+    setCounter(counter+1);
+  }
+  let colorStyle = {color:props['color'],fontSize:props['size']}
+    return(
+      <div style={colorStyle}>
+        React Component
+        <br /><br />
+        <button onClick={increamentCounter}>Click Me!</button>
+        <br /><br />
+        {counter}
+      </div>
+    );
+  }
 export default App;
